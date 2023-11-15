38b46a6b00ba0b1ba49a13257a102f2e03b91aab
Abhinav Gopal <abhinavg@berkeley.edu>
2023-11-15 12:41:42 -0800

    minor changes to route to new path

--- scripts/rubbrband.py
+++ scripts/rubbrband.py
@@ -3,6 +3,8 @@ import gradio as gr
 from modules.processing import Processed
 import requests
 import uuid
+import BytesIO
+import base64
 
 class Script(scripts.Script):
     def title(self):
@@ -17,6 +19,10 @@ class Script(scripts.Script):
     def postprocess(self, p, processed: Processed, rubbrband_api_key):
         for image in processed.images:
             image_id = str(uuid.uuid4())
+        # Convert PIL image to bytes
+            image_bytes = BytesIO()
+            image.save(image_bytes, format="JPEG")
+            base64_encoded = base64.b64encode(image_bytes.getvalue()).decode("utf-8")
             image_json_data = {
                 "api_key": rubbrband_api_key,
                 "prompt": p.prompt,
@@ -29,14 +35,14 @@ class Script(scripts.Script):
                 "height": p.height,
                 "sampler_name": p.sampler_name,
                 "sd_model_name": p.sd_model_name,
-                "image": image,
+                "image": base64_encoded,
                 "is_img2img": self.is_img2img,
                 "image_id": image_id,
             }
             if self.is_img2img:
                 image_json_data["init_images"] = p.init_images
             requests.post(
-                "https://block.rubbrband.com/upload_barth_img",
+                "https://block.rubbrband.com/upload_img_community",
                 json=image_json_data,
             )
         return p
